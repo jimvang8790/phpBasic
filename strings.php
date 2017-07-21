538cc5b8b8a0dbfa8525002aa723fd4576cddcd6
Jim Vang <vangjim15@gmail.com>
2017-07-21 15:59:57 -0500

    adding logic

--- strings.php
+++ strings.php
@@ -55,3 +55,57 @@ var_dump($isReady);
 $isReady = false;
 var_dump($isReady);
 ?>
+
+<!-- Comparison -->
+<?php
+$name = 'Jim Vang';
+$string_one = 'Learning to display ';
+$string_one .= '"Hello ';
+$string_one .= $name;
+$string_one .= '!" to the screen.';
+//$string_one = $string_one . "\n";
+// prepend to a string
+//$string_one = 'I am ' . $string_one;
+echo $string_one;
+
+$isReady = true;
+//var_dump($isReady);
+$isReady = false;
+//var_dump($isReady);
+
+var_dump(1 + "2");
+$a = 10;
+$b = '10';
+var_dump($a == $b);
+var_dump($a === $b);
+var_dump($string_one == 'Learning to display "Hello Jim Vang!" to the screen.');
+?>
+
+<!-- Adding Logic to Your Programs -->
+<?php
+$name = 'Jim Vang';
+$string_one = 'Learning to display ';
+$string_one .= '"Hello ';
+$string_one .= $name;
+$string_one .= '!" to the screen.';
+//$string_one = $string_one . "\n";
+// prepend to a string
+//$string_one = 'I am ' . $string_one;
+echo $string_one;
+
+$isReady = true;
+//var_dump($isReady);
+$isReady = false;
+//var_dump($isReady);
+
+if ($string_one == 'Learning to display "Hello Jim Vang!" to the screen.') {
+  echo 'the values match!';
+}
+else if ($string_one == "") {
+  echo '$string_one is empty';
+}
+else {
+  echo 'the values do not match!';
+}
+
+?>
